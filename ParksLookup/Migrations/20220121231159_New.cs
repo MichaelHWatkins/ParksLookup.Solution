0e138cd48c750ef1f446069d9d6a68d85f3c1d42
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2022-01-21 15:14:02 -0800

    fix the database

---
+++ ParksLookup/Migrations/20220121231159_New.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class New : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Park",
+                table: "Park");
+
+            migrationBuilder.RenameTable(
+                name: "Park",
+                newName: "Parks");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Parks",
+                table: "Parks",
+                column: "ParkId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Parks",
+                table: "Parks");
+
+            migrationBuilder.RenameTable(
+                name: "Parks",
+                newName: "Park");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Park",
+                table: "Park",
+                column: "ParkId");
+        }
+    }
+}
