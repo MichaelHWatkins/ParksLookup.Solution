53dc137da1a9ba869f82f911cdcdc53144b8fbe4
MichaelHWatkins <michaelhugheswatkins@gmail.com>
2022-01-21 16:19:13 -0800

    seed the database

---
+++ ParksLookup/Migrations/20220122000458_SeedData.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "ParkName" },
+                values: new object[] { 1, "Grand Canyon" });
+
+            migrationBuilder.InsertData(
+                table: "Parks",
+                columns: new[] { "ParkId", "ParkName" },
+                values: new object[] { 2, "Mt. Rainer" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Parks",
+                keyColumn: "ParkId",
+                keyValue: 2);
+        }
+    }
+}
--- ParksLookup/Models/ParksLookupContext.cs
+++ ParksLookup/Models/ParksLookupContext.cs
@@ -8,7 +8,15 @@ namespace ParksLookup.Models
             : base(options)
         {
         }
-
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+          builder.Entity<Park>()
+          .HasData(
+              new Park { ParkId = 1, ParkName = "Grand Canyon"},
+              new Park { ParkId = 2, ParkName = "Mt. Rainer"}
+              
+          );
+        }
         public DbSet<Park> Parks { get; set; }
     }
 }
